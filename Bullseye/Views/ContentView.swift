c046dc0ee660d9026eead8d16095088e72818046
Patrik Kántor <patrik.kantor.work@gmail.com>
2023-01-30 17:18:35 +0100

    extracting views

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -18,56 +18,71 @@ struct ContentView: View {
             Color("BackgroundColor")
                 .edgesIgnoringSafeArea(.all)
             VStack {
-                Text("🎯🎯🎯\nPut the Bullseye as close as you can to".uppercased())
-                    .bold()
-                    .kerning(2.0)
-                    .multilineTextAlignment(.center)
-                    .lineSpacing(4.0)
-                    .font(.footnote)
-                    .padding(.leading, 30.0)
-                    .padding(.trailing, 30.0)
-                    .foregroundColor(Color("TextColor"))
-                Text(String(game.target))
-                    .kerning(-1.0)
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                    .foregroundColor(Color("TextColor"))
-                HStack {
-                    Text("1")
-                        .bold()
-                        .foregroundColor(Color("TextColor"))
-                    Slider(value: $sliderValue, in: 1.0...100.0)
-                    Text("100")
-                        .bold()
-                        .foregroundColor(Color("TextColor"))
-                }
-                    .padding()
-                Button {
-                    alertIsVisible = true
-                } label: {
-                    Text("Hit me".uppercased())
-                        .bold()
-                        .font(.title3)
-                }
-                    .alert("Hello there!", isPresented: $alertIsVisible) {
-                      Button("Awesome!") { }
-                    } message: {
-                      let roundedValue = Int(sliderValue.rounded())
-                        Text("The slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round.")
-                    }
-                    .padding(20.0)
-                    .background(
-                        ZStack {
-                            Color("ButtonColor")
-                            LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
-                        }
-                    )
-                    .foregroundColor(Color.white)
-                    .cornerRadius(21.0)
+                InstructionsView(game: $game)
+                SliderView(sliderValue: $sliderValue)
+                HitMeButton(alertIsVisible: $alertIsVisible, sliderValue: $sliderValue, game: $game)
             }
         }
     }
 }
+
+struct InstructionsView: View {
+    @Binding var game: Game
+    
+    var body: some View {
+        VStack {
+            InstructionText(text: "🎯🎯🎯\nPut the Bullseye as close as you can to")
+                .padding(.leading, 30.0)
+                .padding(.trailing, 30.0)
+            BigNumberText(text: String(game.target))
+        }
+    }
+}
+
+struct SliderView: View {
+    @Binding var sliderValue: Double
+    
+    var body: some View {
+        HStack {
+            SliderLabelText(text: "1")
+            Slider(value: $sliderValue, in: 1.0...100.0)
+            SliderLabelText(text: "100")
+        }
+        .padding()
+    }
+}
+
+struct HitMeButton: View {
+    @Binding var alertIsVisible: Bool
+    @Binding var sliderValue: Double
+    @Binding var game: Game
+    
+    var body: some View {
+        Button {
+            alertIsVisible = true
+        } label: {
+            Text("Hit me".uppercased())
+                .bold()
+                .font(.title3)
+        }
+            .alert("Hello there!", isPresented: $alertIsVisible) {
+              Button("Awesome!") { }
+            } message: {
+              let roundedValue = Int(sliderValue.rounded())
+                Text("The slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round.")
+            }
+            .padding(20.0)
+            .background(
+                ZStack {
+                    Color("ButtonColor")
+                    LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
+                }
+            )
+            .foregroundColor(Color.white)
+            .cornerRadius(21.0)
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
